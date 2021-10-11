e75b8cea6e3db07da3a57925d95fa7b3825b48f7
Wesley Kelly <wesley.kelly@hotmail.com>
2021-10-11 18:10:42 +0100

    delete function working as should

--- src/test.js
+++ src/test.js
@@ -1,24 +1,76 @@
 import { forEach } from "lodash";
-
+import { displayNewProject } from "./displayProjectData";
 export const test = () => {
   let button = document.getElementsByClassName("del-prj-btn");
-  const dataToDisplay = JSON.parse(window.localStorage.getItem("Task"));
-  //console.log(dataToDisplay);
+  //takes the array out of local storage
+  let dataToDisplay = JSON.parse(window.localStorage.getItem("Task"));
 
+  // identifies  button from click
   for (let i = 0; i < button.length; i++) {
     button[i].onclick = function (e) {
       let delbutnum = this.id;
 
       let num = parseInt(delbutnum.substr(12) - 1);
-      return num;
-      //console.log(dataToDisplay);
-      //let objectRemove
-      //console.log(arrayToSet);
+      // gets the value num and removes  object at that  index
+      dataToDisplay.splice(num, 1);
+      //sends  the  array minus  the deleted object back to local storage
+      localStorage.setItem("Task", JSON.stringify(dataToDisplay));
+      // removes all projects  from screen
+      const divsToRemove = document.querySelectorAll(".new-prj-box");
+      for (let i = 0; i < divsToRemove.length; i++) {
+        divsToRemove[i].remove();
+      }
+      const placeToDisplayProjects = document.querySelector(".project-bar");
+      // this  repopulates  the screen with the shortend  array
+      for (let j = 0; j < dataToDisplay.length; j++) {
+        const newProjectBox = document.createElement("div");
+        newProjectBox.className = "new-prj-box";
+        newProjectBox.dataset.projectNum = `${j}`;
+        const prjNum = document.createElement("div");
+        const title = document.createElement("div");
+        const descrip = document.createElement("div");
+        const infoBox = document.createElement("div");
+        const buttonBox = document.createElement("div");
+        const addBtn = document.createElement("button");
+        const viewBtn = document.createElement("button");
+        const delBtn = document.createElement("button");
+        infoBox.className = "prj-box-info";
+
+        buttonBox.className = "prj-btn-box";
+
+        addBtn.className = "prj-btn";
+        addBtn.id = "prj-btn-add";
+        addBtn.innerText = "add TO-DO";
+
+        viewBtn.className = "prj-btn";
+        viewBtn.id = "prj-btn-view";
+        viewBtn.innerText = "view";
+
+        delBtn.className = "del-prj-btn";
+        delBtn.id = `prj-btn-del-${j + 1}`;
+        delBtn.innerText = "delete project";
+        delBtn.addEventListener("click", test);
+        delBtn.dataset.btnNum = `${j}`;
+
+        title.className = "displayed-data";
+        descrip.className = "displayed-data";
+        newProjectBox.id = "onscreen";
+        prjNum.innerText = `Project ${j + 1}`;
+        title.innerText = dataToDisplay[j]["title"];
+        descrip.innerText = dataToDisplay[j]["description"];
+
+        placeToDisplayProjects.appendChild(newProjectBox);
+
+        newProjectBox.appendChild(prjNum);
+        prjNum.appendChild(infoBox);
+        infoBox.appendChild(title);
+
+        infoBox.appendChild(descrip);
+        infoBox.insertAdjacentElement("afterend", buttonBox);
+        buttonBox.appendChild(addBtn);
+        buttonBox.appendChild(viewBtn);
+        buttonBox.appendChild(delBtn);
+      }
     };
-    console.log(num);
-    //localStorage.setItem("Task", JSON.stringify(arrayToSet));
   }
 };
-//
-//console.log(arrayToSet);
-// need to make another function to delete
